0c733e1422c3b0d98cabf591ccd105355c1dc77c
chunjung94 <cjswnd12@gmail.com>
2022-04-24 12:08:51 -0700

    Initial Commit

---
+++ music/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  music
+//
+//  Created by 김천중 on 2022/04/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
